8bd3611c2534539dd7df32e102ec21cda8771a21
Azathot11 <87489449+Azathot11@users.noreply.github.com>
2022-12-07 00:40:06 +0100

    6 dec2

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -2,24 +2,38 @@ import React from 'react';
 import {BsTwitter,BsInstagram} from 'react-icons/bs';
 import {FaFacebook} from 'react-icons/fa';
 import {ImLinkedin} from 'react-icons/im';
+import { FacebookShareButton,WhatsappShareButton,LinkedinShareButton,TwitterShareButton } from 'react-share';
 
 
 export const SocialMedia = () => {
+    const linkHandler =(link)=>{
+        window.open(link, "_blank");
+    }
   return (
     <div className="app__social">
-        <div>
-            <FaFacebook/>
-        </div>
-        <div>
-            <BsInstagram/>
-        </div>
-        <div>
-            <ImLinkedin/>
-        </div>
-        <div>
-            <BsTwitter/>
-        </div>
+      <div>
+        <FacebookShareButton
+          url={"https://web.facebook.com/profile.php?id=100088137210911"}
+        >
+          <FaFacebook />
+        </FacebookShareButton>
+      </div>
+      <div>
+        <LinkedinShareButton
+          url={`https://www.linkedin.com/in/paul-nji-bb9294204`}
+        >
+          <ImLinkedin />
+        </LinkedinShareButton>
+        {/* <ImLinkedin onClick={()=>{linkHandler('https://www.linkedin.com/in/paul-nji-bb9294204/')}}/> */}
+      </div>
+      <div>
+        <BsInstagram />
+      </div>
+
+      <div>
+        <BsTwitter />
+      </div>
     </div>
-  )
+  );
 }
 export default SocialMedia
